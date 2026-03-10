af90f8484d56270b41b5fb0a577c9410c629796b
Stig Helge Strøm <stig.helge.strom@nav.no>
2026-03-10 09:57:02 +0100

    Fjernet beskrivelsen og la heller til en kommentar

--- src/main/kotlin/no/nav/infotrygd/kontantstotte/security/Rolle.kt
+++ src/main/kotlin/no/nav/infotrygd/kontantstotte/security/Rolle.kt
@@ -1,10 +1,8 @@
 package no.nav.infotrygd.kontantstotte.security
 
-enum class Rolle(
-    val beskrivelse: String,
-) {
-    FORVALTER("Forvalter"),
-    APPLICATION("Tilgang til applikasjonen"),
+enum class Rolle {
+    FORVALTER, // "Bruker har token med en gruppe som innholder forvalterrolle"
+    APPLICATION, // "Maskin til maskin token med rolle access_as_application"
     ;
 
     fun authority(): String = "ROLE_$name"
